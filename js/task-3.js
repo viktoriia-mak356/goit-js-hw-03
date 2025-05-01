c83e89723042cd191b562f23b7bdfeb0d07dfc00
Viktoriia <v.makovskaya356@gmail.com>
2025-05-02 01:18:38 +0300

    commit

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+function filterArray(numbers, value) {
+    const array3 = [];
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers [i] > value) {
+            array3.push(numbers[i]);
+        }
+    }
+    return array3;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
